b5561c5312a9600d37f5c51a3c2e177bc3b77443
Mrfad <49369275+Mrfad@users.noreply.github.com>
2021-06-07 20:21:58 +0300

    adit and delete post

--- social/views.py
+++ social/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
 from .models import Post
-from .forms import PostForm
+from .forms import CommentForm, PostForm
+from django.views.generic.edit import UpdateView, DeleteView
 
 class PostListView(View):
     def get(self, request, *args, **kwargs):
@@ -27,3 +29,31 @@ class PostListView(View):
             'form': form,
         }
         return render(request, 'social/post_list.html', context)
+
+class PostDetailView(View):
+    def get(self, request, pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        form = CommentForm()
+
+        context = {
+            'post':post,
+            'form':form,
+
+        }
+        return render(request, 'social/post-detail.html', context)
+
+class PostEditView(UpdateView):
+    model = Post
+    fields = ['body']
+    template_name = 'social/post_edit.html'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('post-detail', kwargs={'pk':pk})
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'social/post_delete.html'
+    success_url = reverse_lazy('post-list')
+
+        
